5621e4b2c84ebcf2027fdec7f03a8b65e30cef78
Marek Langiewicz <marek.langiewicz@gmail.com>
2020-08-04 13:43:52 +0200

    play / fun / wip

--- src/main/kotlin/SourceFun.kt
+++ src/main/kotlin/SourceFun.kt
@@ -1,25 +1,74 @@
 package pl.mareklangiewicz
 
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.FileTreeElement
+import org.gradle.api.file.FileVisitDetails
+import org.gradle.api.tasks.*
+import org.gradle.kotlin.dsl.register
+import java.io.File
 
+data class Def(
+    val taskName: String,
+    val sourceDir: String,
+    val outputDir: String,
+    val action: Action<FileTreeElement>
+)
 
 open class SourceFunExtension {
-    // FIXME: list of definitions: (input, output, transformation??) (transformation will be THE FUN)
-    var input = "TODO input file/dir/list??"
-    var output = "TODO output file/dir/list??"
+
+    val defs = mutableListOf<Def>()
+
+    operator fun Def.unaryPlus() = defs.add(this)
 }
 
 class SourceFunPlugin : Plugin<Project> {
     override fun apply(target: Project) {
 
-        val extension = target.extensions.create<SourceFunExtension>("sourceFun", SourceFunExtension::class.java)
+        val extension = target.extensions.create("sourceFun", SourceFunExtension::class.java)
 
-        // FIXME: remove this temporary task definition
-        target.task("sourceFun") {
-            doLast {
-                println("TODO: tasks for sourceFun; input: ${extension.input}; output: ${extension.output}")
+        target.afterEvaluate {// FIXME: is afterEvaluate appropriate here??
+            for (def in extension.defs) target.tasks.register<SourceFunTask>(def.taskName) {
+                source(def.sourceDir)
+                outputDir = target.file(def.outputDir)
+                action = { def.action.execute(this) }
             }
         }
+    }
+}
+
+open class SourceFunTask : SourceTask() {
+
+    @get:OutputDirectory
+    var outputDir: File? = null
+
+    // action should only write file(s) inside outputDir
+    @Internal
+    var action: FileVisitDetails.() -> Unit = {}
+
+    @TaskAction
+    fun execute() {
+        source.visit { action() } // FIXME: do not use "visit" we want user to write explicit loops
+            // TODO: define own MINIMAL (micro) multiplatform abstractions for files and file trees
+    }
+}
+
+
+// FIXME: remove - it's a temporary task class for experiments
+open class SourceRegexTask : SourceTask() {
+
+    @get:OutputDirectory
+    var outputDir: File? = null
+
+    @get:Input
+    var match: String? = null
+
+    @get:Input
+    var replace: String? = null
+
+    @TaskAction
+    fun execute() {
+        println("TODO: SourceRegexTask.execute() source:$source; outputDir: $outputDir; match: $match; replace: $replace")
     }
 }
