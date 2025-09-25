3ed2a29dba1b90e3d1294425be596d3d201dd372
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-09-25 17:19:46 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
   // TODO: check separation between api and engine - so I can do similar in ULog (with separate bridges to CLog etc.)
 }
 
-val kgVer = "0.1.16" // https://central.sonatype.com/artifact/pl.mareklangiewicz/kground/versions
+val kgVer = "0.1.17" // https://central.sonatype.com/artifact/pl.mareklangiewicz/kground/versions
 
 setMyWeirdSubstitutions(
   "kground" to kgVer,
